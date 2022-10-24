af989393f079d18d32b4ec4a7a8a7b4fd40202e2
kenjimiura89 <paulokmiura@gmail.com>
2022-10-24 18:20:20 -0300

    Atividade python

---
+++ SrJoao.py
@@ -0,0 +1,17 @@
+numero1 = int(input('informe a quantidade de 5 centavos'))
+numero2 = int(input('informe a quantidade de 10 centavos'))
+numero3 = int(input('informe a quantidade de 25 centavos'))
+numero4 = int(input('informe a quantidade de 50 centavos'))
+numero5 = int(input('informe a quantidade de 1 real'))
+
+resultado_5centavos = numero1*5
+resultado_10centavos = numero2*10
+resultado_25centavos = numero3*25
+resultado_50centavos = numero4*50
+resultado_1real = numero5*1
+
+print("possui em 5 centavos", resultado_5centavos, "moedas")
+print("possui em 10 centavos", resultado_10centavos, "moedas")
+print("possui em 25 centavos", resultado_25centavos, "moedas")
+print("possui em 50 centavos", resultado_50centavos, "moedas")
+print("possui em 1 Real", resultado_1real, "moedas")
